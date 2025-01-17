77d492c8d3967078a4a339645a67a57c28b9c22d
Gregory Gerard <wako57@gmail.com>
2025-01-17 09:50:39 +0100

    feat: add v-on:click

--- 04-events-starting-code/app.js
+++ 04-events-starting-code/app.js
@@ -4,6 +4,14 @@ const app = Vue.createApp({
       counter: 0,
     };
   },
+  methods: {
+    increment() {
+      this.counter++;
+    },
+    decrement() {
+      this.counter--;
+    },
+  },
 });
 
-app.mount('#events');
+app.mount("#events");
