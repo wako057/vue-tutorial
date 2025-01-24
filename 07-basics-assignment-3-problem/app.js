19aea1a122997119e5de940d78fd50abd74a17a4
Gregory Gerard <wako57@gmail.com>
2025-01-24 14:20:22 +0100

    feat: exercice proposition

---
+++ 07-basics-assignment-3-problem/app.js
@@ -0,0 +1,34 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      result: 0,
+    };
+  },
+  computed: {
+    output() {
+      if (this.result < 37) {
+        return "Not there yet";
+      }
+      if (this.result > 37) {
+        return "Too much!";
+      }
+      return this.result;
+    },
+  },
+  watch: {
+    result(nb) {
+      const that = this;
+      setTimeout(function () {
+        console.log("on reset");
+        that.result = 0;
+      }, 5000);
+    },
+  },
+  methods: {
+    add(nb) {
+      this.result += nb;
+    },
+  },
+});
+
+app.mount("#assignment");
