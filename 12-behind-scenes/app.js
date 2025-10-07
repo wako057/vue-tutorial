7d822e736229ee3134aef414df7a59eef320742a
Gregory Gerard <wako057@gmail.com>
2025-10-07 13:40:19 +0200

    feat: behind the scene setup

---
+++ 12-behind-scenes/app.js
@@ -0,0 +1,28 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      currentUserInput: "",
+      message: "Vue is great!",
+    };
+  },
+  methods: {
+    saveInput(event) {
+      this.currentUserInput = event.target.value;
+    },
+    setText() {
+      this.message = this.currentUserInput;
+    },
+  },
+});
+
+app.mount("#app");
+
+let message = "Hello";
+
+let longMessage = message + " World";
+
+console.log(longMessage);
+
+message = "Hello !!!";
+
+console.log(longMessage);
