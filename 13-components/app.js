70b484b7975446ea67d0543f9b88c5d8094b2bd1
Gregory Gerard <wako057@gmail.com>
2025-10-13 13:35:14 +0200

    feat: add component concept with template

--- 13-components/app.js
+++ 13-components/app.js
@@ -27,4 +27,34 @@ const app = Vue.createApp({
   },
 });
 
+app.component("friend-contact", {
+  template: `
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toggleDetails">
+      {{ detailsAreVisible ? 'Hide' : 'Show' }} Details
+    </button>
+    <ul v-show="detailsAreVisible">
+      <li><strong>Phone:</strong> {{ friend.phone }}</li>
+      <li><strong>Email:</strong> {{ friend.email }}</li>
+    </ul>
+  </li>
+  `,
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+        name: "Manuel Lorenz",
+        phone: "01234 5678 991",
+        email: "manuel@localhost.com",
+      },
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+  },
+});
+
 app.mount("#app");
