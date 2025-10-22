704ec04c1a447f928b87fe1d00d89c05d745786b
Gregory Gerard <wako057@gmail.com>
2025-10-22 13:58:58 +0200

    feat: add first component

---
+++ 15-second-app/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+    <h2>My Friends</h2>
+    <ul>
+        <li v-for="friend of friends" :key="friend.name">
+            <h2>{{ friend.name }}</h2>
+        </li>
+    </ul>
+</template>
+
+<script >
+export default {
+    data() {
+        return {
+            friends: [
+                {
+                    id: "manuel",
+                    name: "Manuel Lorenz",
+                    phone: "01234 5678 991",
+                    email: "manuel@localhost.com",
+                    detailsAreVisible: false,
+                },
+                {
+                    id: "julie",
+                    name: "Julie Jones",
+                    phone: "09876 543 221",
+                    email: "julie@localhost.com",
+                    detailsAreVisible: false,
+                }
+            ]
+        }
+    }
+}
+
+</script>
